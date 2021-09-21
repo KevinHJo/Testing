3114ff58d95c7a33df1a68503cb5b561f4cb0384
Kevin Jo <kevinhjo725@gmail.com>
2021-09-21 16:30:31 -0400

    Add files

---
+++ db/migrate/20210921191941_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :body, null: false
+      t.integer :author_id, null: false
+      t.integer :parent_id
+      t.timestamps
+    end
+
+    add_index :posts, :author_id
+  end
+end
